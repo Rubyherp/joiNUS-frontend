16a8db69dc262652e61e0cff19ede4b352d8180b
XN <horxiangneng123@gmail.com>
2026-05-21 00:35:11 +0800

    add bare min text to individual tabs

---
+++ app/(tabs)/create.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Create() {
+    return (
+        <View className="flex flex-1 justify-center">
+            <Text>Create Page</Text>
+        </View>
+    )
+}
