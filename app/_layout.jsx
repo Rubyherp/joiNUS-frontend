977cf4276f7fd6cc1227ef17238c31bd4b82b1af
XN <horxiangneng123@gmail.com>
2026-05-20 22:31:03 +0800

    rm glueStack.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,11 @@
+import { Screen } from "react-native-screens";
 import "../global.css";
 import { Stack } from "expo-router";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 export default function RootLayout() {
     return (
-        <GluestackUIProvider mode="light">
-            <Stack />
-        </GluestackUIProvider>
-    );
+        <Stack
+            screenOptions={{ headerShown: false }}>
+        </Stack>
+    )
 }
