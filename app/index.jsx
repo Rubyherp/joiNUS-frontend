97f7eb8656148904ab66226344394fa365553192
XN <horxiangneng123@gmail.com>
2026-05-20 23:41:18 +0800

    add some styling to index.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,45 @@
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, Pressable } from 'react-native';
 import Logo from '../assets/images/logo2.png';
 import Spacer from "@/components/themedComponents/spacer";
+import { Link } from 'expo-router';
+
 
 export default function Home() {
     return (
         <View className='flex flex-1 justify-center items-center bg-white'>
-            <Image source={Logo} className='w-80 h-40' resizeMode='contain' />
-            <Text className=''>Hello world</Text>
+            <View
+                className="rounded-2xl overflow-hidden border border-red-500 self-center bg-black px-6 py-3"
+            >
+                <Image
+                    source={Logo}
+                    className="w-80 h-40"
+                    resizeMode="contain"
+                />
+            </View>
+            <Spacer height={60} />
+
+            <Text className='text-2xl text-center font-semibold text-neutral-800'>
+                Your <Text className='text-red-500 italic font-extrabold'>Smartest Team</Text> Formation Platform
+            </Text>
+
+            <Text className='text-2xl text-center italic font-extrabold text-red-500'>
+                Effortlessly <Text className='text-black not-italic font-semibold'> Connect.</Text>
+            </Text>
+            <Spacer />
+
+            <Link href='/login' asChild>
+                <Pressable className='border border-red-500 rounded-3xl p-4 active:opacity-70 bg-black w-80'>
+                    <Text className='font-semibold text-center text-2xl text-white'>Log In</Text>
+                </Pressable>
+            </Link>
+            <Spacer height={20} />
+
+            <Link href='/register' asChild>
+                <Pressable className='border border-red-500 rounded-3xl p-4 active:opacity-70 bg-black w-80'>
+                    <Text className='font-semibold text-center text-2xl text-white'>Register</Text>
+                </Pressable>
+            </Link>
+
         </View >
     )
 }
