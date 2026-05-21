4e7d0ef9ab689cdb5cf43247359417ee476577a8
XN <horxiangneng123@gmail.com>
2026-05-22 02:35:27 +0800

    set up register logic

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,17 +1,37 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity, ActivityIndicator, Alert } from "react-native";
 import { TouchableWithoutFeedback } from "react-native";
 import Logo from '../../assets/images/logo2.png';
-import { Link } from "expo-router";
+import { Link, useRouter } from "expo-router";
+import { useContext, useState } from "react";
+import { UserContext } from "@/context/userContext";
 
 // themed imports
 import Spacer from "@/components/themedComponents/spacer";
 import ThemedInput from "@/components/themedComponents/themedInput";
-import ThemedLink from "@/components/themedComponents/themedLink";
+import { Keyboard } from "react-native";
 
 
-export default function Login() {
+export default function Register() {
+    const { register } = useContext(UserContext);
+    const [registerDetail, setRegisterDetail] = useState({ email: '', password: '' });
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
+
+    const handleSubmit = async () => {
+        setLoading(true);
+
+        try {
+            await register(registerDetail);
+            router.replace('/login');
+        } catch (error) {
+            Alert.alert('Error', error.message);
+        }
+        setLoading(false);
+    }
+
+
     return (
-        <TouchableWithoutFeedback>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <View className="flex flex-1 justify-center items-center">
 
                 <View
@@ -30,17 +50,33 @@ export default function Login() {
                 <ThemedInput
                     className="text-lg border border-black rounded-2xl p-4 w-[80%]"
                     placeholder="EMAIL"
+                    value={registerDetail.email}
+                    onChangeText={(text) => setRegisterDetail(prev => ({ ...prev, email: text }))}
                 />
                 <Spacer height={10} />
 
                 <ThemedInput
                     className="text-lg border border-black rounded-2xl p-4 w-[80%]"
                     placeholder="PASSWORD"
+                    secureTextEntry={true}
+                    value={registerDetail.password}
+                    onChangeText={(text) => setRegisterDetail(prev => ({ ...prev, password: text }))}
                 />
                 <Spacer height={20} />
 
-
-                <ThemedLink href={'/login'} text={'Sign Up!'} style={{ width: "60px" }} />
+                <TouchableOpacity
+                    onPress={handleSubmit}
+                    disabled={loading}
+                    activeOpacity={0.7} // Control the fade opacity when tapped
+                    className={`w-[80%] py-3 rounded-xl justify-center items-center ${loading ? 'bg-blue-400' : 'bg-blue-600'
+                        }`}
+                >
+                    {loading ? (
+                        <ActivityIndicator color="#fff" />
+                    ) : (
+                        <Text className="text-white font-semibold text-sm">Submit</Text>
+                    )}
+                </TouchableOpacity>
                 <Spacer height={10} />
 
                 <Text>
