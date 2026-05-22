1d2f450dd6d6bdadb045105742c96f2c120f3a1c
XN <horxiangneng123@gmail.com>
2026-05-22 22:51:19 +0800

    improve UIUX for login and register page

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -2,10 +2,13 @@
 
 import { View, Text, Image, TouchableOpacity, ActivityIndicator, Alert } from "react-native";
 import { TouchableWithoutFeedback } from "react-native";
-import Logo from '../../assets/images/logo2.png';
+import Logo from '../../assets/images/logo-white.png';
 import { Link, useRouter } from "expo-router";
 import { useContext, useState } from "react";
 import { UserContext } from "@/context/userContext";
+import EmailError from "@/customError/emailError";
+import EmptyPasswordError from "@/customError/emptyPasswordError";
+import { LinearGradient } from "@/components/ui/linear-gradient";
 
 // themed imports
 import Spacer from "@/components/themedComponents/spacer";
@@ -18,34 +21,50 @@ export default function Register() {
     const [loginDetail, setLoginDetail] = useState({ email: '', password: '' });
     const [loading, setLoading] = useState(false);
     const router = useRouter();
+    const [emailError, setEmailError] = useState(null);
+    const [passwordError, setPasswordError] = useState(null);
+
+    const handleError = (error) => {
+        if (error instanceof EmailError) {
+            setEmailError(true);
+        }
+        if (error instanceof EmptyPasswordError) {
+            setPasswordError(true);
+        }
+    }
 
     const handleSubmit = async () => {
         setLoading(true);
-
+        setEmailError(null);
+        setPasswordError(null);
         try {
             await login(loginDetail);
             router.replace('/profile');
         } catch (error) {
-            Alert.alert('Error', error.message);
+            Alert.alert(error.message);
+            handleError(error);
         }
         setLoading(false);
     }
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <View className="flex flex-1 justify-center items-center">
+            {/* <View className="flex flex-1 justify-center items-center bg"> */}
 
-                <View
-                    className="rounded-2xl overflow-hidden border border-red-500 self-center bg-black px-6 py-3"
-                >
-                    <Image
-                        source={Logo}
-                        className="w-80 h-40"
-                        resizeMode="contain"
-                    />
-                </View>
-                <Spacer height={60} />
-                <Text className="text-4xl font-extrabold text-center">Sign Up!</Text>
+            <LinearGradient
+                className="flex-1 items-center justify-center px-8 py-12"
+                colors={['#FF6B6B', '#FFE66D']}
+                start={[0, 0]}
+                end={[1, 1]}
+            >
+
+                <Image
+                    source={Logo}
+                    className="w-90 h-60"
+                    resizeMode="contain"
+                />
+
+                <Text className="text-4xl font-extrabold text-center">Log In to your Account</Text>
                 <Spacer />
 
                 <ThemedInput
@@ -53,6 +72,8 @@ export default function Register() {
                     placeholder="EMAIL"
                     value={loginDetail.email}
                     onChangeText={(text) => setLoginDetail(prev => ({ ...prev, email: text }))}
+                    style={!emailError ? null : { borderColor: 'red', borderWidth: 2 }}
+                    autoCapitalize="none"
                 />
                 <Spacer height={10} />
 
@@ -62,6 +83,8 @@ export default function Register() {
                     secureTextEntry={true}
                     value={loginDetail.password}
                     onChangeText={(text) => setLoginDetail(prev => ({ ...prev, password: text }))}
+                    style={!passwordError ? null : { borderColor: 'red', borderWidth: 2 }}
+                    autoCapitalize="none"
                 />
                 <Spacer height={20} />
 
@@ -69,16 +92,25 @@ export default function Register() {
                     onPress={handleSubmit}
                     disabled={loading}
                     activeOpacity={0.7} // Control the fade opacity when tapped
-                    className={`w-[80%] py-3 rounded-xl justify-center items-center ${loading ? 'bg-blue-400' : 'bg-blue-600'
-                        }`}
+                    className="w-[80%]"
                 >
-                    {loading ? (
-                        <ActivityIndicator color="#fff" />
-                    ) : (
-                        <Text className="text-white font-semibold text-sm">Submit</Text>
-                    )}
+                    <LinearGradient
+                        className={`py-3 rounded-xl justify-center items-center ${loading ? 'bg-blue-400' : 'bg-blue-600'}`}
+                        colors={['#8637CF', '#0F55A1']}
+                        start={[0, 1]}
+                        end={[1, 0]}
+                    >
+                        {loading ? (
+                            <ActivityIndicator color="#fff" />
+                        ) : (
+                            <Text className="text-white font-semibold text-lg">Log In</Text>
+                        )}
+                    </LinearGradient>
                 </TouchableOpacity>
                 <Spacer height={10} />
+
+
+
 
                 <Text>
                     Don't have an Account?
@@ -90,7 +122,8 @@ export default function Register() {
                 <Spacer />
 
 
-            </View>
+                {/* </View> */}
+            </LinearGradient>
         </TouchableWithoutFeedback>
     )
 }
