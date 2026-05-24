87d0596a64cd916d812462767b6984b3649dbc25
XN <horxiangneng123@gmail.com>
2026-05-24 19:14:21 +0800

    create layout for tabs

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,74 @@
+import { View } from "react-native";
+import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+
+export default function TabLayout() {
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false
+            }}
+        >
+            <Tabs.Screen
+                name="landing"
+                options={{
+                    title: "Home",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons name={focused ? 'home' : 'home-outline'} size={24} color='black' />
+                    )
+                }}
+            />
+
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: "Saved",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons name={focused ? 'bookmark' : 'bookmark-outline'} size={24} color='black' />
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="create"
+                options={{
+                    title: "Create",
+                    tabBarIcon: ({ focused }) => (
+                        <View style={{
+                            width: 48,
+                            height: 48,
+                            borderRadius: 26,
+                            backgroundColor: 'black',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            marginBottom: 20,
+                        }}>
+                            <Ionicons name="add" size={30} color={focused ? 'pink' : 'white'} />
+                        </View>
+                    ),
+                }}
+            />
+
+            <Tabs.Screen
+                name="chats"
+                options={{
+                    title: "Chats",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons name={focused ? 'chatbubble' : 'chatbubble-outline'} size={24} color='black' />
+                    )
+                }}
+            />
+
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    tabBarIcon: ({ focused }) => (
+                        <Ionicons name={focused ? 'person' : 'person-outline'} size={24} color='black' />
+                    )
+                }}
+            />
+
+            <Tabs.Screen name="profileSetup" options={{ href: null }} />
+        </Tabs>
+    )
+}
