6f7773de0996835f4bb59981d0df3c1e1bfb2936
XN <horxiangneng123@gmail.com>
2026-06-02 15:58:43 +0800

    add create post design with TODO

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,10 +1,23 @@
-import { View, Text, TouchableOpacity, ActivityIndicator, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native"; import { SafeAreaView } from "react-native-safe-area-context"; import { useState } from "react";
+import { View, Text, TouchableOpacity, Pressable, ActivityIndicator, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native"; import { SafeAreaView } from "react-native-safe-area-context"; import { useState } from "react";
+
 import Spacer from "@/components/themedComponents/spacer";
 import { LinearGradient } from "@/components/ui/linear-gradient";
 import CommunityPicker from "@/components/helpers/communityPicker";
 import ThemedInput from "@/components/themedComponents/themedInput";
 import { Divider } from "@/components/ui/divider";
 import DeadlinePicker from "@/components/helpers/deadlinePicker";
+import { Button, ButtonText, ButtonGroup } from "@/components/ui/button";
+import {
+    Actionsheet,
+    ActionsheetBackdrop,
+    ActionsheetContent,
+    ActionsheetDragIndicator,
+    ActionsheetDragIndicatorWrapper,
+} from '@/components/ui/actionsheet';
+import { Box } from "@/components/ui/box";
+import { Icon, CloseIcon } from '@/components/ui/icon';
+import { UploadCloud } from "lucide-react-native";
+import * as ImagePicker from "expo-image-picker";
 
 //TODO: implement create post logic
 //TODO: upload file with Actionsheet
@@ -19,9 +32,47 @@ export default function Create() {
     const [requirements, setRequirements] = useState("");
     const [memberLimit, setMemberLimit] = useState("");
     const [deadline, setDeadline] = useState("");
+    const [showImageUpload, setShowImageUpload] = useState(false);
 
+    const handleCloseImageUpload = () => {
+        setShowImageUpload(false);
+    }
+
+    //TODO: send backend request to create post
     const handleSubmit = () => {
         setLoading(true);
+    }
+
+    //TODO: implement image upload logic
+    const handleImageUpload = async () => {
+        const result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        })
+
+        if (result.canceled) {
+            return;
+        }
+
+        //TODO: append to formData
+        const image = result.assets[0];
+        const formData = new FormData();
+        formData.append('postFile', {
+
+        })
+
+        //TODO: fetch url
+        //TODO: check logic: should be saved as state? not sending as request directly
+        try {
+            const response = await fetch()
+
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(data.error || "Failed to upload image");
+            }
+            return;
+        } catch (error) {
+            throw error;
+        }
     }
 
     return (
@@ -94,6 +145,61 @@ export default function Create() {
                         />
                         <Divider className="my-4" />
 
+                        {/* image upload */}
+                        <Text className="text-base font-semibold text-gray-800 uppercase tracking-widest mb-1 px-1">Image: (optional)</Text>
+                        <Button onPress={() => setShowImageUpload(true)}>
+                            <ButtonText>Upload</ButtonText>
+                        </Button>
+                        <Actionsheet isOpen={showImageUpload} onClose={handleCloseImageUpload}>
+                            <ActionsheetBackdrop />
+                            <ActionsheetContent className="px-5">
+                                <ActionsheetDragIndicatorWrapper>
+                                    <ActionsheetDragIndicator />
+                                </ActionsheetDragIndicatorWrapper>
+
+                                <View className="flex-row w-full justify-between mb-4">
+                                    <View className="flex gap-2">
+                                        <Text className="text-lg text-white font-bold">Upload your Image</Text>
+                                        <Text className="text-white">JPG, PDF, PNG supported</Text>
+                                    </View>
+
+                                    <View>
+                                        <Pressable onPress={handleCloseImageUpload} className="p-2">
+                                            <Icon
+                                                as={CloseIcon}
+                                                size="lg"
+                                                className="stroke-background-500"
+                                            />
+                                        </Pressable>
+                                    </View>
+                                </View>
+
+                                <Box className="my-[18px] items-center justify-center rounded-xl bg-background-50 border border-dashed border-outline-300 h-[130px] w-full">
+                                    <Icon
+                                        as={UploadCloud}
+                                        className="h-[62px] w-[62px] stroke-background-200"
+                                    />
+                                    <Text className="text-white">No files uploaded yet</Text>
+                                </Box>
+
+                                <ButtonGroup className="w-full">
+                                    <Button className="w-full" onPress={handleImageUpload}>
+                                        <ButtonText>Browse files</ButtonText>
+                                    </Button>
+                                    <Button
+                                        className="w-full"
+                                        variant="outline"
+                                        isDisabled
+                                        action="secondary"
+                                    >
+                                        <ButtonText>Upload</ButtonText>
+                                    </Button>
+                                </ButtonGroup>
+
+                            </ActionsheetContent>
+                        </Actionsheet>
+                        <Divider className="my-4" />
+
                         {/* more details */}
                         <Text className="text-base font-semibold text-gray-800 uppercase tracking-widest mb-1 px-1">More Details: (optional)</Text>
                         <ThemedInput
@@ -135,6 +241,7 @@ export default function Create() {
                             onChangeText={setMemberLimit}
                             value={memberLimit}
                             autoCorrect={false}
+                            keyboardType="numeric"
                             textAlignVertical="top"
                         />
                         <Divider className="my-4" />
@@ -144,11 +251,8 @@ export default function Create() {
                         <DeadlinePicker onSelect={date => setDeadline(date)} />
 
                     </ScrollView>
-
                 </View>
-
             </TouchableWithoutFeedback>
-
-        </SafeAreaView >
+        </SafeAreaView>
     )
 }
