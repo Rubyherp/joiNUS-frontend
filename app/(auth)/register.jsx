88a453acc12477a38045e27b446ad91ef1f2b0a0
XN <horxiangneng123@gmail.com>
2026-06-12 00:39:59 +0800

    improve UX by adding KeyboardAvoidingView

--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,4 +1,4 @@
-import { Text, Image, TouchableOpacity, ActivityIndicator, Alert, useColorScheme } from "react-native";
+import { Text, Image, TouchableOpacity, ActivityIndicator, Alert, useColorScheme, KeyboardAvoidingView, Platform } from "react-native";
 import { TouchableWithoutFeedback } from "react-native";
 import LogoLight from '../../assets/images/logo-white.png';
 import LogoDark from '../../assets/images/logo-gold.png';
@@ -66,72 +66,78 @@ export default function Register() {
                 end={[1, 1]}
             >
 
-                <Image
-                    source={Logo}
-                    className="w-90 h-60"
-                    resizeMode="contain"
-                />
-
-                <Text
-                    className={`text-4xl font-extrabold text-center text-${BWColor}`}
-                >Sign Up!</Text>
-                <Spacer />
-
-                <ThemedInput
-                    className={`text-base text-${BWColor} ${inputBGColor} border-black/10 rounded-2xl w-[80%] h-14 px-4`}
-                    testID="email-input"
-                    placeholder="e_______@u.nus.edu"
-                    value={registerDetail.email}
-                    onChangeText={(text) => setRegisterDetail(prev => ({ ...prev, email: text }))}
-                    style={!emailError ? null : { borderColor: 'red', borderWidth: 2 }}
-                    autoCapitalize="none"
-                    KeyboardType="email-address"
-                    autoCorrect={false}
-                />
-                <Spacer height={10} />
-
-                <ThemedInput
-                    className={`text-base text-${BWColor} ${inputBGColor} border-black/10 rounded-2xl w-[80%] h-14 px-4`}
-                    testID="password-input"
-                    placeholder="PASSWORD"
-                    secureTextEntry={true}
-                    value={registerDetail.password}
-                    onChangeText={(text) => setRegisterDetail(prev => ({ ...prev, password: text }))}
-                    style={!passwordError ? null : { borderColor: 'red', borderWidth: 2 }}
-                    autoCapitalize="none"
-                />
-                <Spacer height={20} />
-
-                <TouchableOpacity
-                    onPress={handleSubmit}
-                    disabled={loading}
-                    activeOpacity={0.7}
-                    className="w-[80%]"
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    className="w-full items-center justify-center"
+                    keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 0}
                 >
-                    <LinearGradient
-                        className={`py-3 rounded-xl justify-center items-center ${loading ? 'bg-blue-400' : 'bg-blue-600'}`}
-                        colors={['#8637CF', '#0F55A1']}
-                        start={[0, 1]}
-                        end={[1, 0]}
+                    <Image
+                        source={Logo}
+                        className="w-90 h-60"
+                        resizeMode="contain"
+                    />
+
+                    <Text
+                        className={`text-4xl font-extrabold text-center text-${BWColor}`}
+                    >Sign Up!</Text>
+                    <Spacer />
+
+                    <ThemedInput
+                        className={`text-base text-${BWColor} ${inputBGColor} border-black/10 rounded-2xl w-[80%] h-14 px-4`}
+                        testID="email-input"
+                        placeholder="e_______@u.nus.edu"
+                        value={registerDetail.email}
+                        onChangeText={(text) => setRegisterDetail(prev => ({ ...prev, email: text }))}
+                        style={!emailError ? null : { borderColor: 'red', borderWidth: 2 }}
+                        autoCapitalize="none"
+                        KeyboardType="email-address"
+                        autoCorrect={false}
+                    />
+                    <Spacer height={10} />
+
+                    <ThemedInput
+                        className={`text-base text-${BWColor} ${inputBGColor} border-black/10 rounded-2xl w-[80%] h-14 px-4`}
+                        testID="password-input"
+                        placeholder="PASSWORD"
+                        secureTextEntry={true}
+                        value={registerDetail.password}
+                        onChangeText={(text) => setRegisterDetail(prev => ({ ...prev, password: text }))}
+                        style={!passwordError ? null : { borderColor: 'red', borderWidth: 2 }}
+                        autoCapitalize="none"
+                    />
+                    <Spacer height={20} />
+
+                    <TouchableOpacity
+                        onPress={handleSubmit}
+                        disabled={loading}
+                        activeOpacity={0.7}
+                        className="w-[80%]"
                     >
-                        {loading ? (
-                            <ActivityIndicator color="#fff" />
-                        ) : (
-                            <Text className="text-lg text-white font-semibold ">Register</Text>
-                        )}
-                    </LinearGradient>
-                </TouchableOpacity>
-                <Spacer height={10} />
+                        <LinearGradient
+                            className={`py-3 rounded-xl justify-center items-center ${loading ? 'bg-blue-400' : 'bg-blue-600'}`}
+                            colors={['#8637CF', '#0F55A1']}
+                            start={[0, 1]}
+                            end={[1, 0]}
+                        >
+                            {loading ? (
+                                <ActivityIndicator color="#fff" />
+                            ) : (
+                                <Text className="text-lg text-white font-semibold ">Register</Text>
+                            )}
+                        </LinearGradient>
+                    </TouchableOpacity>
+                    <Spacer height={10} />
 
-                <Text className={`text-${BWColor}`}>
-                    Already have an Account?
-                    <Link href={'/login'} className="font-bold text-red-500">
-                        <Spacer height={0} width="4" />
-                        Log In!
-                    </Link>
-                </Text>
-                <Spacer />
+                    <Text className={`text-${BWColor}`}>
+                        Already have an Account?
+                        <Link href={'/login'} className="font-bold text-red-500">
+                            <Spacer height={0} width="4" />
+                            Log In!
+                        </Link>
+                    </Text>
+                    <Spacer />
 
+                </KeyboardAvoidingView>
             </LinearGradient>
         </TouchableWithoutFeedback>
     )
